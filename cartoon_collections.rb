a85cab9185b3ffcffe5442087d337c91783cdd95
Michael Roose <mjroose@gmail.com>
2018-08-05 20:19:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |dwarf, index|
     puts "#{index + 1}. #{dwarf}"
@@ -11,6 +13,7 @@ def summon_captain_planet(elements)
 end
 
 def long_planeteer_calls(calls)
+  binding.pry
   long_words = calls.include? do |call|
     call.length > 4
   end
@@ -21,3 +24,6 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+assorted_words = ["two", "go", "industrious", "bop"]
+long_planeteer_calls(assorted_words)
